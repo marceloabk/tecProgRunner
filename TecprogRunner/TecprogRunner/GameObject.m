79f8f7310266d9856bcc84ce83845e22c2e795d4
Marcelo <marceloabk@hotmail.com>
2015-09-15 23:38:03 -0300

    Fixing override methods of gameObject class

--- TecprogRunner/TecprogRunner/GameObject.m
+++ TecprogRunner/TecprogRunner/GameObject.m
@@ -10,24 +10,26 @@
 
 @implementation GameObject
 
-//-(SKTexture *)generateTextureWithName:(NSString *)name{
-//    // Gerar texturas com o nome da imagem
-//}
-//
-//-(SKPhysicsBody *)generatePhysicsBodyWithTextureWithTextureNamed:(NSString *)image{
-//    // Gerar corpo físico usando a textura
-//}
-//
-//-(void)setBasicsAttributes{
-//    // Atributos básicos do tipo GameObject
-//}
-//
-//-(void)beginContactWithNode:(SKNode *)node withBitmask:(uint32_t)bitmask andContact:(SKPhysicsContact *)contact{
-//    // Tratamento para quando começa o contato
-//}
-//
-//-(void)endContactWithNode:(SKNode *)node withBitmask:(uint32_t)bitmask andContact:(SKPhysicsContact *)contact{
-//    // Tratamento para quando termina o contato
-//}
+-(SKTexture *)generateTextureWithName:(NSString *)name{
+    // Gerar texturas com o nome da imagem
+    return [SKTexture textureWithImageNamed:name];
+}
+
+-(SKPhysicsBody *)generatePhysicsBodyWithTextureWithTextureNamed:(NSString *)image{
+    // Gerar corpo físico usando a textura
+    return [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(100, 100)];
+}
+
+-(void)setBasicsAttributes{
+    // Atributos básicos do tipo GameObject
+}
+
+-(void)beginContactWithNode:(SKNode *)node withBitmask:(uint32_t)bitmask andContact:(SKPhysicsContact *)contact{
+    // Tratamento para quando começa o contato
+}
+
+-(void)endContactWithNode:(SKNode *)node withBitmask:(uint32_t)bitmask andContact:(SKPhysicsContact *)contact{
+    // Tratamento para quando termina o contato
+}
 
 @end
