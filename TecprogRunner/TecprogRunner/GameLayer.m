6833714f7cfbafae1fd1b64fdb6bb1b009cd83a0
Marcelo <marceloabk@hotmail.com>
2015-09-23 23:12:05 -0300

    Callig throwProjectile when player tap the right side of screen

--- TecprogRunner/TecprogRunner/GameLayer.m
+++ TecprogRunner/TecprogRunner/GameLayer.m
@@ -40,6 +40,7 @@
         [self.player jump];
     }else if(touchLocation.x > _size.width/2){
         NSLog(@"User clicked on right side of game layer");
+        [self.player throwProjectile];
     }else{
         // Do nothing
     }
