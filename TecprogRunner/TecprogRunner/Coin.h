f7f38acceaeda2c1afff61aeb30360386ed09ed3
Julio Xavier <juliobepid@hotmail.com>
2015-09-24 14:37:58 -0300

    Created Coin class

---
+++ TecprogRunner/TecprogRunner/Coin.h
@@ -0,0 +1,13 @@
+//
+//  Coin.h
+//  TecprogRunner
+//
+//  Created by Julio Xavier on 24/09/15.
+//  Copyright © 2015 Bepid-UnB. All rights reserved.
+//
+
+#import "GameObject.h"
+
+@interface Coin : GameObject
+
+@end
---
+++ TecprogRunner/TecprogRunner/Coin.m
@@ -0,0 +1,13 @@
+//
+//  Coin.m
+//  TecprogRunner
+//
+//  Created by Julio Xavier on 24/09/15.
+//  Copyright © 2015 Bepid-UnB. All rights reserved.
+//
+
+#import "Coin.h"
+
+@implementation Coin
+
+@end
