add731ecd637e1b2cf7598fe70199d2a45daa0c7
Marcelo <marceloabk@hotmail.com>
2015-09-23 23:01:29 -0300

    Creating projectile basics attributes method
    Initializing the projectile with the right sprite

--- TecprogRunner/TecprogRunner/Projectile.m
+++ TecprogRunner/TecprogRunner/Projectile.m
@@ -8,17 +8,29 @@
 
 #import "Projectile.h"
 
+@interface Projectile()
+
+@end
+
 @implementation Projectile
 
 // Initialize the projectile with a position
 -(instancetype) initWithPosition:(CGPoint)position{
-    self = [super init];
+    self = [super initWithTexture:[SKTexture textureWithImageNamed:@"projectileSprite"]];
     if(self != nil){
+        [self setBasicsAttributes];
         
+        self.physicsBody = [self generatePhysicsBody];
+        self.position = position;
     }else{
         // There is no alternative path for this if
     }
     
     return self;
 }
+
+-(void) setBasicsAttributes{
+    self.texture.filteringMode = SKTextureFilteringNearest;
+}
+
 @end
