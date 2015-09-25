b1d63bf493bd0ff1e016938b6085b31f8c31b12a
Lucas Costa <lucascostaa73@gmail.com>
2015-09-25 02:47:33 -0300

    Fixing Bug ground moviment.

--- TecprogRunner/TecprogRunner/BackgroundLayer.m
+++ TecprogRunner/TecprogRunner/BackgroundLayer.m
@@ -14,7 +14,9 @@
     float backgroundDefaultVelocity;
     SKSpriteNode* _firstGround;
     SKSpriteNode* _secondGround;
-    SKSpriteNode* _backgroundClouds;
+    
+    SKSpriteNode* _firstClouds;
+    SKSpriteNode* _secondClouds;
 }
 
 -(instancetype) initWithSize:(CGSize)size{
@@ -34,42 +36,72 @@
         _secondGround = [[SKSpriteNode alloc] initWithColor:green size:backgroundSize];
         _secondGround.anchorPoint = CGPointZero;
         
-        // Setting background sprite initial point
+        //Setting initials Points
+        CGPoint cloudsInitialPoint = CGPointMake(_size.width, 0);
         _initialPoint = CGPointMake(_size.width, -_firstGround.size.height*0.8);
-        _firstGround.position = _initialPoint;
         
+        // Setting background sprite initial point
+        _firstGround.position = _initialPoint;
         _secondGround.position = _initialPoint;
         
         //Instantiating background clouds
-        _backgroundClouds = [[SKSpriteNode alloc] initWithImageNamed:@"background_Clouds"];
-        _backgroundClouds.size = CGSizeMake(2160, _size.height);
-        _backgroundClouds.position = CGPointZero;
-        _backgroundClouds.anchorPoint = CGPointZero;
-        _backgroundClouds.zPosition = -1;
+        _firstClouds = [[SKSpriteNode alloc] initWithImageNamed:@"background_Clouds"];
+        _firstClouds.size = CGSizeMake(2160, _size.height);
+        _firstClouds.position = CGPointZero;
+        _firstClouds.anchorPoint = CGPointZero;
+        _firstClouds.zPosition = -1;
         
+        _secondClouds = [[SKSpriteNode alloc] initWithImageNamed:@"background_Clouds"];
+        _secondClouds.size = CGSizeMake(2160, _size.height);
+        _secondClouds.position = CGPointMake(0, -_size.height);
+        _secondClouds.anchorPoint = CGPointZero;
+        _secondClouds.zPosition = -1;
         
         //Moviment placeholder until tile creation of tile pool do not Delete
+        //Defing moviment time
+        float groundMovimentTime = 8;
+        float cloudMovimentTime = 240;
+        float timeCorrection = 0.02;
+        
         //Setting Background Moviments
         [self setBackgroundMoviment:_firstGround
                         withMoveByX:-_size.width*2
-                   withInitialPoint:_initialPoint andTime:12];
+                   withInitialPoint:_initialPoint andTime:groundMovimentTime];
         
-        [self runAction:[SKAction waitForDuration:5.98] completion:^{
+        [self setBackgroundMoviment:_firstClouds
+                        withMoveByX:-(_firstClouds.size.width*2)
+                   withInitialPoint:cloudsInitialPoint andTime:cloudMovimentTime];
+        
+        [self runAction:[SKAction waitForDuration:groundMovimentTime*0.5 - timeCorrection] completion:^{
             [self setBackgroundMoviment:_secondGround
                             withMoveByX:-_size.width*2
-                       withInitialPoint:_initialPoint andTime:12];
+                       withInitialPoint:_initialPoint andTime:groundMovimentTime];
+        }];
+    
+        [self runAction:[SKAction waitForDuration:cloudMovimentTime*0.5 - timeCorrection] completion:^{
+            
+            _secondClouds.position = cloudsInitialPoint;
+            [self setBackgroundMoviment:_secondClouds
+                            withMoveByX:-(_secondClouds.size.width*2)
+                       withInitialPoint:cloudsInitialPoint andTime:cloudMovimentTime];
         }];
         
-        [self setBackgroundMoviment:_backgroundClouds
-                        withMoveByX:-_backgroundClouds.size.width
-                   withInitialPoint:_initialPoint andTime:240];
+        SKSpriteNode* ground = [[SKSpriteNode alloc] initWithColor:green size:backgroundSize];
+        ground.anchorPoint = CGPointZero;
+        ground.position = CGPointMake(0, -ground.size.height*0.8);
+        
+        [ground runAction:[SKAction moveByX:-ground.size.width y:0 duration:groundMovimentTime]];
+
         
         // Adding clouds to view
-        [self addChild:_backgroundClouds];
+        [self addChild:_firstClouds];
+        [self addChild:_secondClouds];
         
         // Adding background to view
+        [self addChild:ground];
         [self addChild:_firstGround];
         [self addChild:_secondGround];
+
         
     }else{
         // Throw exception
