dc8da57dc946f40bdc1210e2be47aea755073c8c
Julio Xavier <juliobepid@hotmail.com>
2015-09-24 22:23:03 -0300

    BackgroundLayer according to StyleSheet and using some good practices on initWithSize

--- TecprogRunner/TecprogRunner/BackgroundLayer.m
+++ TecprogRunner/TecprogRunner/BackgroundLayer.m
@@ -13,30 +13,33 @@
     float backgroundDefaultVelocity;
 }
 
-- (instancetype)initWithSize:(CGSize)size{
+-(instancetype) initWithSize:(CGSize)size{
     self = [super init];
-    if(self){
+    if(self != NULL){
         
         _size = size;
         self.background = [[SKSpriteNode alloc] initWithColor:[UIColor magentaColor] size:CGSizeMake(size.width, size.height/2)];
         
         self.background.anchorPoint = CGPointZero;
         
-        //Setting background sprite initial point
+        // Setting background sprite initial point
         CGPoint initialPoint = CGPointMake(_size.width, 0);
         self.background.position = initialPoint;
         
-        //Setting backgroundMoviment
+        // Setting backgroundMoviment
         SKAction* backgroundMoviment = [SKAction moveByX:-self.background.size.width*2 y:0 duration:6];
         SKAction* setBackgroundPosition = [SKAction moveTo:initialPoint duration:0.0];
         SKAction* backgroundMovimentSequence = [SKAction sequence:@[backgroundMoviment, setBackgroundPosition]];
         
-        //Running background moviment sequence
+        // Running background moviment sequence
         [self.background runAction:[SKAction repeatActionForever:backgroundMovimentSequence] withKey:@"backgroundMoviment"];
         
-        //Adding background to view
+        // Adding background to view
         [self addChild:self.background];
+    }else{
+        // Throw exception
     }
+    
     return self;
 }
 
