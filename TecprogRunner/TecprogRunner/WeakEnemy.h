4401cf387895d0e8766c5ae364ab7b6032d097a1
Marcelo <marceloabk@hotmail.com>
2015-09-28 08:40:03 -0300

    Creating weakEnemy class

---
+++ TecprogRunner/TecprogRunner/WeakEnemy.h
@@ -0,0 +1,13 @@
+//
+//  WeakEnemy.h
+//  TecprogRunner
+//
+//  Created by Marcelo Cristiano Araujo Silva on 28/09/15.
+//  Copyright © 2015 Bepid-UnB. All rights reserved.
+//
+
+#import "Enemy.h"
+
+@interface WeakEnemy : Enemy
+
+@end
---
+++ TecprogRunner/TecprogRunner/WeakEnemy.m
@@ -0,0 +1,13 @@
+//
+//  WeakEnemy.m
+//  TecprogRunner
+//
+//  Created by Marcelo Cristiano Araujo Silva on 28/09/15.
+//  Copyright © 2015 Bepid-UnB. All rights reserved.
+//
+
+#import "WeakEnemy.h"
+
+@implementation WeakEnemy
+
+@end
