736222f10dfd83bfd7cfe5bb4bda6190d9699335
Julio Xavier <juliobepid@hotmail.com>
2015-10-05 11:13:50 -0300

    Adjusting GameObject according to documentation

--- TecprogRunner/TecprogRunner/GameObject.m
+++ TecprogRunner/TecprogRunner/GameObject.m
@@ -25,6 +25,7 @@
 }
 
 -(SKTexture *) generateTextureWithImageNamed:(NSString *)image{
+    
     // Generate a texture with the image
     SKTexture *imageTexture = [SKTexture textureWithImageNamed:image];
     
@@ -32,8 +33,11 @@
 }
 
 -(SKPhysicsBody *) generatePhysicsBodyWithImageNamed:(NSString *)image{
+    
     // Generate a physics body with the image
     SKTexture *imageTexture = [self generateTextureWithImageNamed:image];
+    
+    // Initializing and setting physicsBody
     SKPhysicsBody *physicsBody = [SKPhysicsBody bodyWithTexture:imageTexture size:self.size];
     physicsBody.allowsRotation = NO;
     
@@ -41,20 +45,17 @@
 }
 
 -(SKPhysicsBody *) generatePhysicsBody{
-    // Generate a physics body
     SKPhysicsBody *physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(100, 100)];
     
     return physicsBody;
 }
 
-    // Set attributes for the default "GameObject"
 -(void) setBasicsAttributes{
     
 }
 
 #pragma mark Defining Contact methods
 
-    // Handling for when the contact starts
 -(void) beginContactWithNode:(SKNode *)node withBitmask:(uint32_t)bitmask andContact:(SKPhysicsContact *)contact{
     
     // Getting body names
@@ -63,8 +64,9 @@
     
     NSLog(@"Contact started between %@ and %@", bodyA, bodyB);
 }
-    // Handling for when the contact ends
 -(void) endContactWithNode:(SKNode *)node withBitmask:(uint32_t)bitmask andContact:(SKPhysicsContact *)contact{
+    
+    // Getting body names
     NSString *bodyA = contact.bodyA.node.name;
     NSString *bodyB = contact.bodyB.node.name;
     
