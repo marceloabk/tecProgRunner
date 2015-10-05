f986f45363b3479f6d32ff994dc6679dfaabf517
Marcelo <marceloabk@hotmail.com>
2015-10-05 10:42:42 -0300

    Creating WeakEnemy Init

--- TecprogRunner/TecprogRunner/WeakEnemy.m
+++ TecprogRunner/TecprogRunner/WeakEnemy.m
@@ -10,4 +10,19 @@
 
 @implementation WeakEnemy
 
+-(instancetype)initWithPosition:(CGPoint)position{
+    NSAssert(!CGPointEqualToPoint(position, CGPointZero) , @"Weak enemy initialized with a 0.0 point");
+    
+    SKTexture *weakEnemyTexture = [SKTexture textureWithImageNamed:@"weakEnemyTexture"];
+    self = [super initWithTexture:weakEnemyTexture];
+    
+    if (self) {
+        
+    }else{
+        // There is no alternative path for this if
+    }
+    
+    return self;
+}
+
 @end
