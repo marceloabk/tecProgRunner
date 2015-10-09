a87e645dca9fc23b00e14329c60355991d5337a9
Lucas Costa <lucascostaa73@gmail.com>
2015-10-09 10:57:41 -0300

    Creating physics controller to deal with physics world, basicaly updating velocities, applying gravity...

---
+++ TecprogRunner/TecprogRunner/PhysicsController.h
@@ -0,0 +1,24 @@
+//
+//  PhysicsController.h
+//  TecprogRunner
+//
+//  Created by Lucas Araujo on 10/9/15.
+//  Copyright © 2015 Bepid-UnB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "GameObject.h"
+
+@interface PhysicsController : NSObject
+
+/**
+ Array of bodies inside physics world
+*/
+@property (nonatomic) NSMutableArray<GameObject*> *bodies;
+
+/**
+ Each frame physics controller updates physics world, applying bodies velocities.
+*/
+-(void) update:(CFTimeInterval)currentTime;
+
+@end
