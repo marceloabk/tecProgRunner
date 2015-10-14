488c96599225d3b9227b0c60f359ed22115fe50b
Marcelo <marceloabk@hotmail.com>
2015-10-14 20:55:29 -0300

    Creating Strong Enemy Class

---
+++ TecprogRunner/TecprogRunner/StrongEnemy.m
@@ -0,0 +1,13 @@
+//
+//  StrongEnemy.m
+//  TecprogRunner
+//
+//  Created by Marcelo Cristiano Araujo Silva on 14/10/15.
+//  Copyright © 2015 Bepid-UnB. All rights reserved.
+//
+
+#import "StrongEnemy.h"
+
+@implementation StrongEnemy
+
+@end
