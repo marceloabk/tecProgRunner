57ef9ed8dce7108e035ba7c1e61602f64266d881
Marcelo <marceloabk@hotmail.com>
2015-10-14 21:54:22 -0300

    Creating Enemy Generator initializer

--- TecprogRunner/TecprogRunner/EnemyGenerator.m
+++ TecprogRunner/TecprogRunner/EnemyGenerator.m
@@ -7,7 +7,22 @@
 //
 
 #import "EnemyGenerator.h"
+#import "StrongEnemy.h"
+#import "WeakEnemy.h"
 
 @implementation EnemyGenerator
 
+// Initialize EnemyGenerator with a position
+-(instancetype)init{
+    
+    self = [super init];
+    if(self != nil){
+        DebugLog(@"Enemy generator initialized with texture successfully");
+        
+    }else{
+        DebugLog(@"Enemy generator can't be initialized");
+    }
+    
+    return self;
+}
 @end
