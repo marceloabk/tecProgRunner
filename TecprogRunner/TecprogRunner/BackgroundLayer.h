0f4eeb2ce573fd137d5cbec9bd4190866346de4c
Julio Xavier <juliobepid@hotmail.com>
2015-10-22 11:37:19 -0200

    More object orientations and good practices at BackgroundLayer

--- TecprogRunner/TecprogRunner/BackgroundLayer.h
+++ TecprogRunner/TecprogRunner/BackgroundLayer.h
@@ -6,6 +6,13 @@
 //
 //  Copyright (c) 2015 Group 8 - Tecprog 2/2015. All rights reserved.
 
+
+#define GREEN_COLOR [UIColor greenColor]
+#define MAGENTA_COLOR [UIColor magentaColor]
+#define GROUND_MOVIMENT_TIME 8
+#define CLOUD_MOVIMENT_TIME 240
+#define TIME_CORRECTION 0.02
+
 #import "BasicLayer.h"
 
 @interface BackgroundLayer : SKNode <LayerRequirements>
--- TecprogRunner/TecprogRunner/BackgroundLayer.m
+++ TecprogRunner/TecprogRunner/BackgroundLayer.m
@@ -9,6 +9,7 @@
 #import "BackgroundLayer.h"
 
 @implementation BackgroundLayer{
+    
     CGSize _size;
     CGPoint _initialPoint;
     float backgroundDefaultVelocity;
@@ -21,76 +22,22 @@
 
 -(instancetype) initWithSize:(CGSize)size{
     self = [super init];
+    
     if(self != NULL){
         
         _size = size;
         
-        UIColor *magenta = [UIColor magentaColor];
-        UIColor *green = [UIColor greenColor];
-        CGSize backgroundSize = CGSizeMake(size.width, size.height/2);
+        CGSize backgroundSize = CGSizeMake(_size.width,_size.height/2);
         
-        // Setting grounds
-        _firstGround = [[SKSpriteNode alloc] initWithColor:magenta size:backgroundSize];
-        _firstGround.anchorPoint = CGPointZero;
+        [self setGroundsWithBackgroundSize:backgroundSize];
+        [self setClouds];
+        [self setAllBackgroundMoviment];
         
-        _secondGround = [[SKSpriteNode alloc] initWithColor:green size:backgroundSize];
-        _secondGround.anchorPoint = CGPointZero;
-        
-        //Setting initials Points
-        CGPoint cloudsInitialPoint = CGPointMake(_size.width, 0);
-        _initialPoint = CGPointMake(_size.width, -_firstGround.size.height*0.8);
-        
-        // Setting background sprite initial point
-        _firstGround.position = _initialPoint;
-        _secondGround.position = _initialPoint;
-        
-        //Instantiating background clouds
-        _firstClouds = [[SKSpriteNode alloc] initWithImageNamed:@"background_Clouds"];
-        _firstClouds.size = CGSizeMake(2160, _size.height);
-        _firstClouds.position = CGPointZero;
-        _firstClouds.anchorPoint = CGPointZero;
-        _firstClouds.zPosition = -1;
-        
-        _secondClouds = [[SKSpriteNode alloc] initWithImageNamed:@"background_Clouds"];
-        _secondClouds.size = CGSizeMake(2160, _size.height);
-        _secondClouds.position = CGPointMake(0, -_size.height);
-        _secondClouds.anchorPoint = CGPointZero;
-        _secondClouds.zPosition = -1;
-        
-        //Moviment placeholder until tile creation of tile pool do not Delete
-        //Defing moviment time
-        float groundMovimentTime = 8;
-        float cloudMovimentTime = 240;
-        float timeCorrection = 0.02;
-        
-        //Setting Background Moviments
-        [self setBackgroundMoviment:_firstGround
-                        withMoveByX:-_size.width*2
-                   withInitialPoint:_initialPoint andTime:groundMovimentTime];
-        
-        [self setBackgroundMoviment:_firstClouds
-                        withMoveByX:-(_firstClouds.size.width*2)
-                   withInitialPoint:cloudsInitialPoint andTime:cloudMovimentTime];
-        
-        [self runAction:[SKAction waitForDuration:groundMovimentTime*0.5 - timeCorrection] completion:^{
-            [self setBackgroundMoviment:_secondGround
-                            withMoveByX:-_size.width*2
-                       withInitialPoint:_initialPoint andTime:groundMovimentTime];
-        }];
-    
-        [self runAction:[SKAction waitForDuration:cloudMovimentTime*0.5 - timeCorrection] completion:^{
-            
-            _secondClouds.position = cloudsInitialPoint;
-            [self setBackgroundMoviment:_secondClouds
-                            withMoveByX:-(_secondClouds.size.width*2)
-                       withInitialPoint:cloudsInitialPoint andTime:cloudMovimentTime];
-        }];
-        
-        SKSpriteNode* ground = [[SKSpriteNode alloc] initWithColor:green size:backgroundSize];
+        SKSpriteNode* ground = [[SKSpriteNode alloc] initWithColor:GREEN_COLOR size:backgroundSize];
         ground.anchorPoint = CGPointZero;
         ground.position = CGPointMake(0, -ground.size.height*0.8);
         
-        [ground runAction:[SKAction moveByX:-ground.size.width y:0 duration:groundMovimentTime]];
+        [ground runAction:[SKAction moveByX:-ground.size.width y:0 duration:GROUND_MOVIMENT_TIME]];
 
         
         // Adding clouds to view
@@ -127,6 +74,66 @@
     
     // Running background moviment sequence
     [background runAction:repeatMovimentForever];
+}
+
+-(void) setAllBackgroundMoviment{
+    
+    CGPoint cloudsInitialPoint = CGPointMake(_size.width, 0);
+    
+    [self setBackgroundMoviment:_firstGround
+                    withMoveByX:-_size.width*2
+               withInitialPoint:_initialPoint andTime:GROUND_MOVIMENT_TIME];
+    
+    [self setBackgroundMoviment:_firstClouds
+                    withMoveByX:-(_firstClouds.size.width*2)
+               withInitialPoint:cloudsInitialPoint andTime:CLOUD_MOVIMENT_TIME];
+    
+    [self runAction:[SKAction waitForDuration:GROUND_MOVIMENT_TIME*0.5 - TIME_CORRECTION] completion:^{
+        [self setBackgroundMoviment:_secondGround
+                        withMoveByX:-_size.width*2
+                   withInitialPoint:_initialPoint andTime:GROUND_MOVIMENT_TIME];
+    }];
+    
+    [self runAction:[SKAction waitForDuration:CLOUD_MOVIMENT_TIME*0.5 - TIME_CORRECTION] completion:^{
+        
+        _secondClouds.position = cloudsInitialPoint;
+        [self setBackgroundMoviment:_secondClouds
+                        withMoveByX:-(_secondClouds.size.width*2)
+                   withInitialPoint:cloudsInitialPoint andTime:CLOUD_MOVIMENT_TIME];
+    }];
+}
+
+// Instantiating background ground
+-(void) setGroundsWithBackgroundSize:(CGSize)backgroundSize{
+    
+    _firstGround = [[SKSpriteNode alloc] initWithColor:MAGENTA_COLOR size:backgroundSize];
+    _firstGround.anchorPoint = CGPointZero;
+    
+    _secondGround = [[SKSpriteNode alloc] initWithColor:GREEN_COLOR size:backgroundSize];
+    _secondGround.anchorPoint = CGPointZero;
+    
+    _initialPoint = CGPointMake(_size.width, -_firstGround.size.height*0.8);
+    
+    // Setting background sprite initial point
+    _firstGround.position = _initialPoint;
+    _secondGround.position = _initialPoint;
+
+}
+
+
+// Instantiating background Clouds
+-(void) setClouds{
+    _firstClouds = [[SKSpriteNode alloc] initWithImageNamed:@"background_Clouds"];
+    _firstClouds.size = CGSizeMake(2160, _size.height);
+    _firstClouds.position = CGPointZero;
+    _firstClouds.anchorPoint = CGPointZero;
+    _firstClouds.zPosition = -1;
+    
+    _secondClouds = [[SKSpriteNode alloc] initWithImageNamed:@"background_Clouds"];
+    _secondClouds.size = CGSizeMake(2160, _size.height);
+    _secondClouds.position = CGPointMake(0, -_size.height);
+    _secondClouds.anchorPoint = CGPointZero;
+    _secondClouds.zPosition = -1;
 }
 
 @end
