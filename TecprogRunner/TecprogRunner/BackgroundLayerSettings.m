e31e4d9dd460f14b7749ad5483354121b2f8d79c
Henrique <hlopes13@hotmail.com>
2015-10-25 23:37:01 -0200

    Settings layer enhanced, design added, images added, gameData settings data added, saved properly, buttons in settings layer being pulled from gameData

--- TecprogRunner/TecprogRunner/BackgroundLayerSettings.m
+++ TecprogRunner/TecprogRunner/BackgroundLayerSettings.m
@@ -21,7 +21,7 @@
 
 -(void) addBackgroundSprite{
     
-    self.background = [SKSpriteNode spriteNodeWithTexture:[SKTexture textureWithImageNamed:@"background"]];
+    self.background = [SKSpriteNode spriteNodeWithTexture:[SKTexture textureWithImageNamed:@"backgroundSettings"]];
     self.background.position =CGPointZero;
     self.background.anchorPoint = CGPointZero;
     [self.background setScale:0.5];
--- TecprogRunner/TecprogRunner/SettingsLayer.m
+++ TecprogRunner/TecprogRunner/SettingsLayer.m
@@ -7,6 +7,7 @@
 //  Copyright (c) 2015 Group 8 - Tecprog 2/2015. All rights reserved.
 
 #import "SettingsLayer.h"
+#import "GameData.h"
 
 @implementation SettingsLayer
 
@@ -41,6 +42,46 @@
 -(void) loadButtons{
     
     [self loadBack];
+    [self loadSoundEffects];
+    [self loadMusic];
+}
+
+-(void) loadSoundEffects{
+
+    NSString *soundEffects;
+    
+    if([GameData sharedGameData].soundEffectsAvailibility){
+        soundEffects = @"volumeOn";
+    }
+    else{
+        soundEffects = @"volumeOff";
+    }
+
+    self.musicButton = [SKSpriteNode spriteNodeWithImageNamed:soundEffects];
+    self.musicButton.position = CGPointMake(142, 375-150);
+    self.musicButton.anchorPoint = CGPointMake(0, 1);
+    self.musicButton.name = @"musicButton";
+    self.musicButton.zPosition = 2;
+    [self.musicButton setScale:0.5];
+}
+
+-(void) loadMusic{
+
+    NSString *music;
+
+    if([GameData sharedGameData].musicAvailibility){
+        music = @"volumeOn";
+    }
+    else{
+        music = @"volumeOff";
+    }
+    self.soundEffectsButton = [SKSpriteNode spriteNodeWithImageNamed:music];
+    self.soundEffectsButton.position = CGPointMake(432, 375-150);
+    self.soundEffectsButton.anchorPoint = CGPointMake(0, 1);
+    self.soundEffectsButton.name = @"soundEffectsButton";
+    self.soundEffectsButton.zPosition = 2;
+    [self.soundEffectsButton setScale:0.5];
+
 }
 
 -(void) loadBack{
@@ -58,6 +99,8 @@
 
     [self.layer addChild:self.backgroundLayerSettings];
     [self.layer addChild:self.backButton];
+    [self.layer addChild:self.musicButton];
+    [self.layer addChild:self.soundEffectsButton];
 
 }
 
