fb60a561e0a14f0056e809dd15262bc2a23d9f9b
Marcelo <marceloabk@hotmail.com>
2015-11-27 00:04:21 -0200

    Creating GameOver class

---
+++ TecprogRunner/TecprogRunner/GameOver.h
@@ -0,0 +1,13 @@
+//
+//  GameOver.h
+//  TecprogRunner
+//
+//  Created by Marcelo Cristiano Araujo Silva on 27/11/15.
+//  Copyright © 2015 Bepid-UnB. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameOver : SKNode
+
+@end
---
+++ TecprogRunner/TecprogRunner/GameOver.m
@@ -0,0 +1,13 @@
+//
+//  GameOver.m
+//  TecprogRunner
+//
+//  Created by Marcelo Cristiano Araujo Silva on 27/11/15.
+//  Copyright © 2015 Bepid-UnB. All rights reserved.
+//
+
+#import "GameOver.h"
+
+@implementation GameOver
+
+@end
